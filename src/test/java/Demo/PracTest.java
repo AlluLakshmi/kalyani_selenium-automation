e578afa2dda43dc50a15a3d8ff23216d6d87904b
hp <hp@DESKTOP-AEKSNLV>
2025-12-18 07:44:19 +0530

    Firsrtimecommit

---
+++ src/test/java/Demo/PracTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+	@Test(groups = "regression")
+	public void carTest() 
+	{
+		System.out.println("Suzuki");
+
+}
+}
---
+++ src/test/java/Demo/Pract1Test.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Pract1Test {
+	@Test(groups = "smoke")
+	public void carTest() 
+	{
+		System.out.println("Audi");
+	}
+	
+
+}
